8d1a581db74ae00c24ad98d33db7459dd26bcce7
ugurakcora <ugur.akcora@pingpong.university>
2025-11-21 05:18:14 +0300

    Configure project for Cloudflare Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+  distDir: 'out',
+}
 
 module.exports = nextConfig
 
